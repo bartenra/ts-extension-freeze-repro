4c2fbedf2becde4f34515bb5029c7f4235cdfa1b
Bart Louwers <bartenra@gmail.com>
2021-05-25 13:26:31 +0200

    first commit

---
+++ src/Dialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <Dialog :open="isOpen" @close="setIsOpen" as="div">
+    <DialogOverlay />
+
+    <DialogTitle>Deactivate account</DialogTitle>
+    <DialogDescription>This will permanently deactivate your account</DialogDescription>
+
+    <p>
+      Are you sure you want to deactivate your account? All of your data will be
+      permanently removed. This action cannot be undone.
+    </p>
+
+    <button @click="setIsOpen(false)">Deactivate</button>
+    <button @click="setIsOpen(false)">Cancel</button>
+  </Dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import {
+  Dialog,
+  DialogOverlay,
+  DialogTitle,
+  DialogDescription,
+} from "@headlessui/vue";
+
+export default defineComponent({
+  components: { Dialog, DialogOverlay, DialogTitle, DialogDescription },
+  setup() {
+    let isOpen = ref(true);
+
+    return {
+      isOpen,
+      setIsOpen(value: boolean) {
+        isOpen.value = value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/Modal.vue
@@ -0,0 +1,56 @@
+<template>
+  <TransitionRoot as="template" :show="open">
+    <Dialog as="div" static :open="open" @close="$emit('close-modal')">
+      <div>
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="ease-in duration-200"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <DialogOverlay />
+        </TransitionChild>
+
+        <TransitionChild
+          as="template"
+          enter="ease-out duration-300"
+          enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enter-to="opacity-100 translate-y-0 sm:scale-100"
+          leave="ease-in duration-200"
+          leave-from="opacity-100 translate-y-0 sm:scale-100"
+          leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        >
+          <div>hello</div>
+        </TransitionChild>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script lang="ts">
+import {
+  Dialog,
+  DialogOverlay,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue';
+
+export default {
+  components: {
+    Dialog,
+    DialogOverlay,
+    TransitionChild,
+    TransitionRoot,
+  },
+  props: {
+    open: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['close-modal'],
+};
+</script>
